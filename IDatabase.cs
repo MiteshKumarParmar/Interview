c04b0d4f09c847a4b38d007fdb52fb9f76936a58
MiteshKumarParmar <95952693+MiteshKumarParmar@users.noreply.github.com>
2021-12-11 19:01:11 +0530

    Add files via upload

---
+++ IDatabase.cs
@@ -0,0 +1,11 @@
+﻿namespace PointsBet.Test
+{
+    public interface IDatabase
+    {
+        void SaveFixture(Fixture newFixtureToBeAdded);
+
+        Fixture GetFixture(string id);
+
+        bool FixtureExists(string id);
+    }
+}
---
+++ IValidateFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointsBet.Test
+{
+    public interface IValidateFixture
+    {
+        FixtureValidationResult Validate(Fixture fixtureToValidate);
+
+        bool IsFixtureAlreadyCreated(string id);
+    }
+
+    public enum FixtureValidationResult
+    {
+        ValidFixture,
+        InValidFixture
+    };
+}
