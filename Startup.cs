d5f5ef6dff12e3ccf47d86fd3a68d48dacee96de
MiteshKumarParmar <95952693+MiteshKumarParmar@users.noreply.github.com>
2021-12-11 18:56:55 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PointsBet.Test;
+
+namespace PointsBet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient<IValidateFixture>();
+            services.AddSingleton<IDatabaseConnector>();    
+            //Need to further add polly extensions for all controllers to have retry mechanisms in place.
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
