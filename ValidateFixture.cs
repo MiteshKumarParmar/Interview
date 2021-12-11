1886e06b88f6f695f80c4a50843e441ed83fe7e9
MiteshKumarParmar <95952693+MiteshKumarParmar@users.noreply.github.com>
2021-12-11 18:59:49 +0530

    Add files via upload

---
+++ ValidateFixture.cs
@@ -0,0 +1,16 @@
+﻿namespace PointsBet.Test
+{
+    public class ValidateFixture : IValidateFixture
+    {
+        public FixtureValidationResult Validate(Fixture fixtureToValidate)
+        {
+            return FixtureValidationResult.ValidFixture;
+        }
+
+        public bool IsFixtureAlreadyCreated(string id)
+        {
+           //Write logic to check db if already a fixture with this ID exists
+           return true;
+        }
+    }
+}
